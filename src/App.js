63ad08673a1372b6486e822ccce57b460ac804c8
Iskak Mantyubetov <ayzik55@gmail.com>
2021-05-28 00:46:42 -0400

    created App component and added styling

--- src/App.js
+++ src/App.js
@@ -1,51 +1,58 @@
-
 import sharansImg from './SharanLeventhal007col.jpg'
 import './App.css';
-import {LIST_OF_PIECES as LIST }from './data.js'
+import {LIST_OF_PIECES as LIST, SUMMARY }from './data.js'
+
+
 
 function App() {
+  LIST.sort(function(a, b){
+    let aa = a.premier_date.split('/').reverse().join();
+    let bb = b.premier_date.split('/').reverse().join();
+    return aa < bb ? -1 : (aa > bb ? 1 : 0);
+});
 
   return (
     <div className="App">
       <header className="App-header">
         Duo Project
-      </header>
-      
+      </header>     
       <main className='main-container'>
         <section>
           <img alt="logo" src={sharansImg}/>
-          <p className="opening-summary">
-            The duo project began in 2015 as part of the first Sharan Leventhal and Friends Studio Recital. Every year Sharan and her students commission and premiere new works for this event.
-          </p>
-          <p className="opening-summary">
-            As of 2020, premieres include thirty-one violin duets, five marimba and violin duets, one trio for violin, marimba and oud, one violin, piano and picture book piece, one violin quartet, one violin quintet with bowed chimes and one work for eight violins written collaboratively by the members of the studio.
-          </p>
-          <p className="opening-summary">
-            The recitals are informal and interactive, and often include interdisciplinary elements such as dance and visual art. The musicians are extremely grateful to all the wonderful contributing composers, most of whom are Boston Conservatory at Berklee and Berklee College of Music faculty, students and alumni.
-          </p>
-          <p className="opening-summary">
-            The pieces are currently being recorded with the support of a grant from the Berklee College of Music
-          </p>
+          {
+            SUMMARY.map(summary => {
+              return (
+                <p key={summary.id} className="opening-summary">{summary.part}</p>
+              )
+            })
+          }
         </section>
-
         <section>
           <ul className="composers-list">
-            <h4>List of peices by composers:</h4>
-            <li>
-              <div className="piece_header">
-                <p>{LIST.title}</p>
-                <a href={LIST.link} target='_blank' rel="noreferrer">{LIST.composer}</a>
-              </div>
-              <p><span>Instrumentation:</span> {LIST.instrumentation}</p>
-              <p>Premier date: {LIST.premier_date}</p>
-              <p>Description: {LIST.notes}</p>
-              <p>Premier performers: {LIST.premier_performers}</p>
-            </li>
+          <h4>List of peices by composers:</h4>
+            {LIST.map(piece => {
+                return (
+                        <li key={piece.id}>
+                          <div className="piece_header">
+                            <p className="piece-title">{piece.title}</p>
+                            {piece.link.length ? <a href={piece.link} target='_blank' rel="noreferrer">{piece.composer}</a> : <p className="piece_composer">{piece.composer}</p>}
+                          </div>
+                          <p><span>Instrumentation:</span> {piece.instrumentation}</p>
+                          <p><span>Premier date</span>: {piece.premier_date}</p>
+                          <p><span>Description</span>: {piece.notes}</p>
+                          <p><span>Premier performers</span>: {piece.premier_performers}</p>
+                        </li>
+                )
+              })
+            }
           </ul>
         </section>
       </main>
       <footer>
-        Copyright 2021 by Duo Project. All Rights Reserved.
+        <div>
+          <p>Contact: <a href="mailto: dilfitz14@gmail.com">Dillon Robb</a></p>
+          <p>Copyright 2021 by Duo Project. All Rights Reserved.</p>
+        </div>
       </footer>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,10 +1,141 @@
-export const LIST_OF_PIECES = 
+export const SUMMARY = [
     {
+        id: '1-summary',
+        part: 'The duo project began in 2015 as part of the first Sharan Leventhal and Friends Studio Recital. Every year Sharan and her students commission and premiere new works for this event.'
+    },
+    {
+        id: '2-summary',
+        part: 'As of 2020, premieres include thirty-one violin duets, five marimba and violin duets, one trio for violin, marimba and oud, one violin, piano and picture book piece, one violin quartet, one violin quintet with bowed chimes and one work for eight violins written collaboratively by the members of the studio.'
+    },
+    {
+        id: '3-summary',
+        part: 'The recitals are informal and interactive, and often include interdisciplinary elements such as dance and visual art. The musicians are extremely grateful to all the wonderful contributing composers, most of whom are Boston Conservatory at Berklee and Berklee College of Music faculty, students and alumni.'
+    },
+    {
+        id: '4-summary',
+        part: 'The pieces are currently being recorded with the support of a grant from the Berklee College of Music.'
+    }
+]
+export const LIST_OF_PIECES = [
+    {
+        id: 1,
         composer: 'Marti Epstein',
         title: 'Bow',
         instrumentation: '2 singing violinists',
-        notes: 'extended techniques, singing and playing, Hocketing, poetry, Judson Evans',
+        notes: 'Extended techniques, singing and playing, Hocketing, poetry, Judson Evans',
         link: 'https://martiepstein.com/',
         premier_date: '02/24/2015',
         premier_performers: 'SL, Lara Ciaccio'
-    }
+    },
+    {
+        id: 2,
+        composer: 'David Vess',
+        title: 'Daydreamer, scratching at surfaces',
+        instrumentation: 'Violin duo',
+        notes: 'Melodic, polyphony, sul ponticello',
+        link: 'https://www.davidvessmusic.com',
+        premier_date: '02/25/2016',
+        premier_performers: 'SL, David Rubin'
+    },
+    {
+        id: 3,
+        composer: 'Peter Fletcher',
+        title: 'December Portrait',
+        instrumentation: 'Violin quartet',
+        notes: 'Tonal, harmonics, barriolage, polyphony',
+        link: '',
+        premier_date: '03/05/2019',
+        premier_performers: 'Malachi Provenzano, Letian Feng, Yinfei Li, SL'
+    },
+    {
+        id: 4,
+        composer: 'Francine Trester',
+        title: 'Dualities',
+        instrumentation: 'Violin duo',
+        notes: 'Poetry, intermediate',
+        link: 'https://www.francinetrester.com/',
+        premier_date: '02/28/2018',
+        premier_performers: 'SL, Alec Norkey'
+    },
+    {
+        id: 5,
+        composer: 'John McDonald',
+        title: 'Duet Quintet',
+        instrumentation: 'Violin duo',
+        notes: 'Melodic, tonal, intermediate',
+        link: 'https://www.johndmcdonaldmusic.com/',
+        premier_date: '03/29/2017',
+        premier_performers: 'SL, Kim Busic'
+    },
+    {
+        id: 6,
+        composer: 'Jonathan Bailey Holland',
+        title: 'Follow',
+        instrumentation: 'Violin duo',
+        notes: 'Canon, intermediate, microtonal, variations',
+        link: 'http://www.jonathanbaileyholland.com/',
+        premier_date: '03/29/2017',
+        premier_performers: 'SL, Sophia Bellino'
+    },
+    {
+        id: 7,
+        composer: 'Aaron Jay Myers',
+        title: 'For The World Is Hollow and I Have Touched The Sky',
+        instrumentation: 'Violin duo',
+        notes: '',
+        link: 'http://www.aaronjaymyers.com/',
+        premier_date: '03/29/2017',
+        premier_performers: 'SL, Nicole Parks'
+    },
+    {
+        id: 8,
+        composer: 'Eun Young Lee',
+        title: 'Geo-Ul',
+        instrumentation: 'Violin duo',
+        notes: 'advanced, hocketing, extended techniques',
+        link: 'https://www.eunyoungleemusic.com/',
+        premier_date: '03/29/2017',
+        premier_performers: 'SL, Dillon Robb'
+    },
+    {
+        id: 9,
+        composer: 'Aaron Jay Myers',
+        title: 'For The World Is Hollow and I Have Touched The Sky',
+        instrumentation: 'Violin duo',
+        notes: '',
+        link: 'http://www.aaronjaymyers.com/',
+        premier_date: '02/28/2018',
+        premier_performers: 'SL, Alec Norkey'
+    },
+    {
+        id: 10,
+        composer: 'Aaron Jay Myers',
+        title: 'For The World Is Hollow and I Have Touched The Sky',
+        instrumentation: 'Violin duo',
+        notes: '',
+        link: 'http://www.aaronjaymyers.com/',
+        premier_date: '02/28/2018',
+        premier_performers: 'SL, Alec Norkey'
+    },
+    {
+        id: 11,
+        composer: 'Aaron Jay Myers',
+        title: 'For The World Is Hollow and I Have Touched The Sky',
+        instrumentation: 'Violin duo',
+        notes: '',
+        link: 'http://www.aaronjaymyers.com/',
+        premier_date: '02/28/2018',
+        premier_performers: 'SL, Alec Norkey'
+    },
+    {
+        id: 12,
+        composer: 'Aaron Jay Myers',
+        title: 'For The World Is Hollow and I Have Touched The Sky',
+        instrumentation: 'Violin duo',
+        notes: '',
+        link: 'http://www.aaronjaymyers.com/',
+        premier_date: '02/28/2018',
+        premier_performers: 'SL, Alec Norkey'
+    },
+]
+    
