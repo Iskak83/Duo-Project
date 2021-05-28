47dbd47180716bda2ae3fc01d7baf75a68febab2
Iskak Mantyubetov <ayzik55@gmail.com>
2021-05-28 13:13:07 -0400

    created App component and added styling

--- src/data.js
+++ src/data.js
@@ -82,7 +82,7 @@ export const LIST_OF_PIECES = [
         composer: 'Aaron Jay Myers',
         title: 'For The World Is Hollow and I Have Touched The Sky',
         instrumentation: 'Violin duo',
-        notes: '',
+        notes: 'n/a',
         link: 'http://www.aaronjaymyers.com/',
         premier_date: '03/29/2017',
         premier_performers: 'SL, Nicole Parks'
@@ -92,7 +92,7 @@ export const LIST_OF_PIECES = [
         composer: 'Eun Young Lee',
         title: 'Geo-Ul',
         instrumentation: 'Violin duo',
-        notes: 'advanced, hocketing, extended techniques',
+        notes: 'Advanced, hocketing, extended techniques',
         link: 'https://www.eunyoungleemusic.com/',
         premier_date: '03/29/2017',
         premier_performers: 'SL, Dillon Robb'
@@ -114,7 +114,7 @@ export const LIST_OF_PIECES = [
         instrumentation: 'Violin duo',
         notes: 'Melodic, barriolage',
         link: 'https://www.docwallacemusic.com/',
-        premier_date: 'n/a',
+        premier_date: 'TBA',
         premier_performers: 'n/a'
     },
     {
@@ -136,6 +136,206 @@ export const LIST_OF_PIECES = [
         link: 'https://www.newmusicusa.org/profile/jimdalton/#:~:text=James%20Dalton%20is%20a%20composer,Michael%20Newman%20and%20John%20Abercrombie.',
         premier_date: '02/25/2016',
         premier_performers: 'SL, Dillon Robb'
+    },
+    {
+        id: 13,
+        composer: 'Cody McVey',
+        title: 'Ocean Reflects Sky',
+        instrumentation: 'Violin duo',
+        notes: 'Extended techniques, barriolage',
+        link: 'https://codymcvey.com/',
+        premier_date: '02/28/2018',
+        premier_performers: 'SL, Kimberly Busic'
+    },
+    {
+        id: 14,
+        composer: 'Alan Mackwell',
+        title: 'Rustic Grace',
+        instrumentation: 'Violin duo',
+        notes: 'Fiddle',
+        link: 'https://soundcloud.com/alanmackwell',
+        premier_date: '03/05/2019',
+        premier_performers: 'SL, Brian Stultigross'
+    },
+    {
+        id: 15,
+        composer: 'Mischa Salkind-Pearl',
+        title: 'Twelve Canons',
+        instrumentation: 'Violin duo',
+        notes: 'Microtonal, canon',
+        link: 'http://mischasalkindpearl.com',
+        premier_date: '02/25/2016',
+        premier_performers: 'SL, Haley Leach'
+    },
+    {
+        id: 16,
+        composer: 'Andy Vores',
+        title: 'Something',
+        instrumentation: 'Soprano & 4 violins',
+        notes: 'n/a',
+        link: 'http://www.andyvores.com/Home/andyvores.home.html',
+        premier_date: '02/25/2016',
+        premier_performers: 'Natalie Hunt, Dillon Robb, David Rubin, Sophia Bellino, Haley Leach'
+    },
+    {
+        id: 17,
+        composer: 'John Murphree',
+        title: 'Under the Wave off Kanagawa',
+        instrumentation: 'Violin quintet with bowed chimes',
+        notes: 'Bowed chimes',
+        link: 'https://bostonconservatory.berklee.edu/directory/john-murphree',
+        premier_date: '02/25/2016',
+        premier_performers: 'SL, Dillon Robb'
+    },
+    {
+        id: 18,
+        composer: 'Stephen Dembski',
+        title: 'Stuck Together',
+        instrumentation: 'Violin duo',
+        notes: 'Tonal, melodic',
+        link: 'http://stephendembski.com/',
+        premier_date: '03/29/2017',
+        premier_performers: 'SL, Dillon Robb'
+    },
+    {
+        id: 19,
+        composer: 'Scott Wheeler',
+        title: 'Sleeping on a Wire',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'https://scottwheeler.org/',
+        premier_date: 'TBA',
+        premier_performers: 'n/a'
+    },  
+    {
+        id: 20,
+        composer: 'John Murphree',
+        title: 'Bitter Salt',
+        instrumentation: 'Violin duo with bowed chimes',
+        notes: 'Bowed chimes',
+        link: 'https://bostonconservatory.berklee.edu/directory/john-murphree',
+        premier_date: '02/28/2018',
+        premier_performers: 'SL, Kara Cantele'
+    },
+    {
+        id: 21,
+        composer: 'William So',
+        title: 'Duo for Violin and Cello',
+        instrumentation: 'Violin & cello',
+        notes: 'n/a',
+        link: 'https://soundcloud.com/wl-so',
+        premier_date: '02/28/2018',
+        premier_performers: 'Alec Norkey, Brianna Tagliaferro'
+    },
+    {
+        id: 22,
+        composer: 'Alexander LaCava',
+        title: 'Violin Duet',
+        instrumentation: 'Violin duo',
+        notes: 'Melodic',
+        link: '',
+        premier_date: '02/28/2018',
+        premier_performers: 'Kimberly Busic, Kara Cantele'
+    },
+    {
+        id: 23,
+        composer: 'Luis Zanforlin',
+        title: 'Portuguese East Tiles',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'http://luiszanforlin.com/',
+        premier_date: '02/28/2018',
+        premier_performers: 'Sophia Bellino, Alec Norkey'
+    },
+    {
+        id: 24,
+        composer: 'Constantinos Søren Demetriou',
+        title: 'Jasmine',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: '',
+        premier_date: '03/03/2020',
+        premier_performers: 'SL, Jasmine Gomez'
+    },
+    {
+        id: 25,
+        composer: 'Peter Fletcher',
+        title: 'Violin Duet',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: '',
+        premier_date: '03/03/2020',
+        premier_performers: 'SL, Malachi Provenzano'
+    },
+    {
+        id: 26,
+        composer: 'Joshua Hahn',
+        title: 'Simulacrum 2',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'http://hahnmusic.com/',
+        premier_date: '03/05/2019',
+        premier_performers: 'SL, Alec Norkey'
+    },
+    {
+        id: 27,
+        composer: 'Michael Laster',
+        title: 'Tevya Tries to Tango',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'http://michaellaster.com/',
+        premier_date: '03/03/2020',
+        premier_performers: 'SL, Clara Mazo'
+    },
+    {
+        id: 28,
+        composer: 'Domenic Salerni',
+        title: 'Party of II',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'http://domenicsalerni.com/',
+        premier_date: 'TBA',
+        premier_performers: 'n/a'
+    },
+    {
+        id: 29,
+        composer: 'Andy Vores',
+        title: 'Fabrication 20: Assemblage',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'http://www.andyvores.com/Home/andyvores.home.html',
+        premier_date: '02/24/2015',
+        premier_performers: 'SL, David Rubin'
+    },
+    {
+        id: 30,
+        composer: 'Jeffrey Madry',
+        title: 'Glassware for modern and baroque violin',
+        instrumentation: 'Violin duo',
+        notes: 'Baroque violin',
+        link: '',
+        premier_date: '02/24/2015',
+        premier_performers: 'David Rubin, Dillon Robb'
+    },
+    {
+        id: 31,
+        composer: 'Matthias Vecanski',
+        title: 'Isolate for Two Violins',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'https://soundcloud.com/mvecanskimusic/',
+        premier_date: 'TBA',
+        premier_performers: 'n/a'
+    },
+    {
+        id: 32,
+        composer: 'Ludsen Martinus',
+        title: 'Meditación',
+        instrumentation: 'Violin duo',
+        notes: 'n/a',
+        link: 'https://soundcloud.com/mvecanskimusic',
+        premier_date: 'TBA',
+        premier_performers: 'n/a'
     },
 ]
     
